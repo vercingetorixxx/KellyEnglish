8aec511c4e80c5cd773390428544a8cf7b2b5bea
vercingetorixxx <ohchanwu@proton.me>
2025-06-25 18:01:27 +0900

    box edits

--- JS/script.js
+++ JS/script.js
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const options = {
         root: null, // Use viewport as root
         rootMargin: "0px", // Margin around root
-        threshold: 0.3 // Trigger when 30% of the target is visible
+        threshold: 0.1 // Trigger when 10% of the target is visible
     };
 
     const observer = new IntersectionObserver(callback, options);
